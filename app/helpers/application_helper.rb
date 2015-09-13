8fad307f468af87bdec5ecb0d7f77644994e3973
Spencer Dixon <spencercdixon@gmail.com>
2015-09-13 13:30:57 -0400

    Added a page_title helper for views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def body_class
     "#{controller.controller_name} #{controller.action_name}"
   end
+
+  def page_title(name)
+    content_for(:title) { name }
+    content_tag('h1', name)
+  end
 end
