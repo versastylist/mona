fb55d312cf58d83a3e49cfb67262ea13b9e1090f
Spencer Dixon <spencercdixon@gmail.com>
2015-09-23 06:54:05 -0400

    Index page for service products so stylists can see what types of appointments clients can book

--- app/controllers/stylists_controller.rb
+++ app/controllers/stylists_controller.rb
@@ -1,5 +1,6 @@
 class StylistsController < ApplicationController
   def show
     @stylist = StylistDecorator.new(User.from_params(params[:id]))
+    @service_products = @stylist.service_products
   end
 end
