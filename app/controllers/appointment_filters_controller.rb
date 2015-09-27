90c15df85518c6811fd37cc0e136bdb4b4c174b9
Spencer Dixon <spencercdixon@gmail.com>
2015-09-27 18:09:39 -0400

    Can now search service products using the price slider

--- app/controllers/appointment_filters_controller.rb
+++ app/controllers/appointment_filters_controller.rb
@@ -2,12 +2,15 @@ class AppointmentFiltersController < ApplicationController
   def index
     @service_menu = ServiceMenu.find(params[:menu_filter_id])
     if params[:query]
+      lower_bound, upper_bound = params[:price_range].split(',')
+
       @service_products = ServiceProduct.search(
         params[:query],
         page: params[:page],
         per_page: 2,
         where: {
-          service_menu: [@service_menu.name]
+          service_menu: [@service_menu.name],
+          price: {gt: lower_bound, lt: upper_bound}
         }
       )
     end
