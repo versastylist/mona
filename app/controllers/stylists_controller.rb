3bd662d7b1337b2940003ded6cd078c41dc0854b
Spencer Dixon <spencercdixon@gmail.com>
2015-10-06 20:02:25 -0400

    Added pagination to the service products for client view

--- app/controllers/stylists_controller.rb
+++ app/controllers/stylists_controller.rb
@@ -1,6 +1,8 @@
 class StylistsController < ApplicationController
   def show
     @stylist = StylistDecorator.new(User.from_params(params[:id]))
-    @service_products = @stylist.service_products.decorate
+    @service_products = ServiceProductDecorator.decorate_collection(
+      @stylist.service_products.page(params[:page]).per(15)
+    )
   end
 end
