e2f7d9f5f06e043be06f19182b05e90c497409cc
Spencer Dixon <spencercdixon@gmail.com>
2015-10-10 18:06:48 -0400

    Populating calendar with real data

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,53 +1,15 @@
 $(function() {
   // Full Calendar
-  var myCalendar = $('#calendar').fullCalendar({
+  var myCalendar = $('#appointmentCalendar').fullCalendar({
       header: {
         left: 'prev,next today',
         center: 'title',
         right: 'month,agendaWeek,agendaDay'
       },
       defaultDate: '2014-06-12',
-      defaultView: 'agendaDay',
+      defaultView: 'agendaWeek',
       editable: false,
-      events: [
-        {
-          title: 'All Day Event',
-          start: '2014-06-01'
-        },
-        {
-          title: 'Long Event',
-          start: '2014-06-07',
-          end: '2014-06-10'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: '2014-06-09T16:00:00'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: '2014-06-16T16:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2014-06-12T10:30:00',
-          end: '2014-06-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: '2014-06-12T12:00:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: '2014-06-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: '2014-06-28'
-        }
-      ],
+      events: $('#availableAppointments').data().appointments,
       eventClick: function(calEvent, jsEvent, view) {
 
           alert('Event: ' + calEvent.title);
--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,9 @@
 class AppointmentsController < ApplicationController
   def new
     @stylist = User.find(params[:stylist_id])
-    @available_appointments = AvailabilityChecker.new(@stylist, current_order.total_time).find_times
+    @available_appointments = AvailabilityChecker.new(
+      @stylist, current_order.total_time
+    ).find_times.to_json
   end
 
   def create
