7ce823eb10d93d7024605762803ed3ab56a35d0a
Spencer Dixon <spencercdixon@gmail.com>
2015-11-07 13:59:38 -0500

    Moved javascript logic to be in more meaningful file

--- app/assets/javascripts/schedule_creation.js
+++ app/assets/javascripts/schedule_creation.js
@@ -29,5 +29,58 @@ $(function() {
     offColor: 'danger',
     onSwitchChange: handleWeekDaySwitchChange
   });
+
+  // Timepickers for schedules
+  $('#new_schedule').on('cocoon:after-insert', function(e, insert) {
+    $('.interval-start-timepicker').timepicker('setTime', '11:30 AM');
+    $('.interval-end-timepicker').timepicker('setTime', '12:30 PM');
+  });
+
+  $('.start-timepicker').each(function(weekDay) {
+    if (!this.value) {
+      $(this).timepicker('setTime', '9:00 AM')
+    } else {
+      var time = moment.utc(this.value);
+      var formattedTime = time.format('h:mm a')
+      $(this).timepicker('setTime', formattedTime);
+    }
+  });
+
+  $('.end-timepicker').each(function(weekDay) {
+    if (!this.value) {
+      $(this).timepicker('setTime', '5:00 PM')
+    } else {
+      var time = moment.utc(this.value);
+      var formattedTime = time.format('h:mm a')
+      $(this).timepicker('setTime', formattedTime);
+    }
+  });
+
+
+  // Edit schedule
+  $('.edit_schedule').on('cocoon:after-insert', function(e, insert) {
+    $(insert).find('.interval-start-timepicker').timepicker('setTime', '11:30 AM');
+    $(insert).find('.interval-end-timepicker').timepicker('setTime', '12:30 PM');
+  });
+
+  $('.interval-start-timepicker').each(function(weekDay) {
+    if (!this.value) {
+      $(this).timepicker('setTime', '11:30 AM')
+    } else {
+      var time = moment.utc(this.value);
+      var formattedTime = time.format('h:mm a')
+      $(this).timepicker('setTime', formattedTime);
+    }
+  });
+
+  $('.interval-end-timepicker').each(function(weekDay) {
+    if (!this.value) {
+      $(this).timepicker('setTime', '12:30 PM')
+    } else {
+      var time = moment.utc(this.value);
+      var formattedTime = time.format('h:mm a')
+      $(this).timepicker('setTime', formattedTime);
+    }
+  });
 });
 
