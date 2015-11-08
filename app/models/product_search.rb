a2123d7235560938807dd3e8c05434b1fb6b4390
Spencer Dixon <spencercdixon@gmail.com>
2015-11-07 19:57:03 -0500

    Added product search migration to fix heroku database

--- app/models/product_search.rb
+++ app/models/product_search.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: product_searches
+#
+#  id         :integer          not null, primary key
+#  term       :string
+#  client_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class ProductSearch < ActiveRecord::Base
   belongs_to :client, class_name: 'User', foreign_key: 'client_id'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107141110) do
+ActiveRecord::Schema.define(version: 20151108005436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
