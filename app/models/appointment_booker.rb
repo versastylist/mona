ff4c97f75af534f3552b0cc9dca43b1064f3e6a1
Spencer Dixon <spencercdixon@gmail.com>
2015-11-08 10:04:57 -0500

    Added 15 min buffer to appointments with new clients.  Fixed breaking time tests by using Timecop

--- app/models/appointment_booker.rb
+++ app/models/appointment_booker.rb
@@ -21,10 +21,19 @@ class AppointmentBooker
 
     week_day.time_intervals.create!(
       start_time: start_time,
-      end_time: end_time,
+      end_time: interval_end_time,
       title: "Appointment Booking with client: #{client.id}",
       appointment_id: appointment.id,
     )
     order.complete!
+  end
+
+  # First time booking add 15 minutes
+  def interval_end_time
+    if client.has_seen_stylist?(stylist)
+      DateTime.parse(end_time)
+    else
+      DateTime.parse(end_time) + 15.minutes
+    end
   end
 end
