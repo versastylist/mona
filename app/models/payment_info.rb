d0604684c0b5284369a650cca250cdebdfd93d8a
Spencer Dixon <spencercdixon@gmail.com>
2015-11-10 18:22:08 -0500

    Trying to use card even though its a bank token

--- app/models/payment_info.rb
+++ app/models/payment_info.rb
@@ -25,9 +25,8 @@ class PaymentInfo < ActiveRecord::Base
       Rails.logger.error("stripe token was: #{stripe_bank_token}")
       customer = Stripe::Customer.create(
         description: "Created by VersaStylist on #{Date.today}",
-        source: stripe_bank_token,
-        email: user.email,
-        name: user.full_name
+        card: stripe_bank_token,
+        email: user.email
       )
     end
     self.stripe_customer_token = customer.id
