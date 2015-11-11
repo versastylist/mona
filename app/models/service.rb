c364106fb2a2c2b6c8c0e2f8228b0f6d4c84c519
Spencer Dixon <spencercdixon@gmail.com>
2015-11-10 19:15:32 -0500

    Fixed css for creating services that fail due to validation issues

--- app/models/service.rb
+++ app/models/service.rb
@@ -11,10 +11,14 @@
 
 class Service < ActiveRecord::Base
   has_many :service_products
-  belongs_to :stylist, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :stylist,
+    class_name: 'User',
+    foreign_key: 'user_id'
   belongs_to :service_menu
 
-  accepts_nested_attributes_for :service_products, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :service_products,
+    reject_if: :all_blank,
+    allow_destroy: true
 
   validates :service_menu_id,
     presence: true
