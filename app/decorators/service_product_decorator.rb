f46bd63169b0dd756da463082ec02f67deca3cf9
Spencer Dixon <spencercdixon@gmail.com>
2015-11-18 16:43:36 -0500

    Added alert on search results to indicate whether or not stylist is active

--- app/decorators/service_product_decorator.rb
+++ app/decorators/service_product_decorator.rb
@@ -6,6 +6,14 @@ class ServiceProductDecorator < ApplicationDecorator
     object.stylist.avatar_url(size)
   end
 
+  def stylist_availability
+    if object.stylist.has_current_schedule?
+      content_tag(:span, 'Active', class: 'label label-success')
+    else
+      content_tag(:span, 'Inactive', class: 'label label-danger')
+    end
+  end
+
   def stylist_link
     link_to object.stylist.username, stylist_path(object.stylist)
   end
