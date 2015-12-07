7c3b98540671c0e9c51fc645a9a597001999550e
lucius fox <mr.captain.awesome@gmail.com>
2015-12-06 22:27:04 -0500

    modified config.action_mailer.smtp_settings for confirmation emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
+  
 =begin
   config.action_mailer.smtp_settings = {
     port: "587",
@@ -97,6 +98,6 @@ Rails.application.configure do
     authentication:       :login,
     enable_starttls_auto: true
   }
-  
+
   config.action_mailer.default_url_options = { host: "https://versastylist.herokuapp.com" }
 end
